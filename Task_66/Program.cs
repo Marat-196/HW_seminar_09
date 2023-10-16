d9a918b3add8b3fe8a9832da14a2b6b30d768e7d
Marat <zhumataev.marat@bk.ru>
2023-10-16 15:23:23 +0500

    add Task 66

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -7,17 +7,23 @@
 
 Console.Write("Введите число M: ");
 int numberM = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите число N: ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-if (numberM < 1 && numberN < 1)
+if (numberM < 1)
 {
     Console.WriteLine("Некорректный ввод!");
     return;
 }
 
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberN < 1)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+
 Console.WriteLine($"M = {numberM}; N = {numberN} -> {SumNatureNumbers(numberM, numberN)}");
+
 
 int SumNatureNumbers(int numM, int numN)
 {
